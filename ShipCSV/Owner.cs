213233dfa983ee84c5c7ab34b84e2a9e1c475a73
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-17 11:02:49 +0100

    Initial commit

---
+++ ShipCSV/Owner.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ShipCSV
+{
+    public class Owner
+    {
+        public string Name;
+        public int ShipCount;
+        public Ship Ship;
+        public Ship SecondShip;
+        public static List<Owner> Owners = new List<Owner>();
+
+        public Owner(string ownerName, Ship ship)
+        {
+            Name = ownerName;
+            Ship = ship;
+            Owners.Add(this);
+            ShipCount = 0;
+        }
+
+        public Owner CountShipsPerOwner()
+        {
+            foreach (var owner in Owners)
+            {
+                if (owner.Name == Name) ShipCount++;
+            }
+            return this;
+        }
+
+        public void AddOwner()
+        {
+            Owners.Add(this);
+        }
+
+        public void RemoveOwner()
+        {
+            Owners.Remove(this);
+        }
+
+        public Ship AddSecondShip()
+        {
+            foreach (var owner in Owners)
+            {
+                if (Name == owner.Name)
+                {
+                    SecondShip = owner.Ship;
+                }
+            }
+            return SecondShip;
+        }
+    }
+}
---
+++ ShipCSV/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;    
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ShipCSV
+{
+    public class Program
+    {
+        /// <summary>
+        /// This assignment involves dynamically reading and handling data from a file, in order to sort out which of the owners
+        /// own more than one ship, including the names of these ships. See ShipTest1.txt for more info.
+        /// A part of the challenge is avoiding LINQ-expressions (Groupby would be suitable here), and practicing 
+        /// the formulation of a sorting algorithm. I formulated the algorithm from my own head, and as this was my first doing so..
+        /// I suspect it can be improved quite a bit.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            var ships = new List<Ship>();
+            using (var sr = new StreamReader("ShipTest1.txt"))
+            {
+                while (true)
+                {
+                    var line = sr.ReadLine();
+                    if (line == null) break;
+                    var row = line.Split(',');
+                    var shipYard = new ShipYard(row);
+                    var ship = shipYard.CreateShip();
+                    var owner = shipYard.CreateOwner(ship);
+                    var countedOwner = owner.CountShipsPerOwner();
+                    ship.Owner = countedOwner; //two-way object reference set for print/WriteLine
+                    if (countedOwner.ShipCount == 2) // when there're two instances in Owners:
+                    {
+                        ships.Add(countedOwner.Ship); //add current/first ship instance
+                        countedOwner.RemoveOwner(); //remove to isolate remaining instance
+                        var secondShip = countedOwner.AddSecondShip();
+                        ships.Add(secondShip); //adds remaining ship
+                        countedOwner.AddOwner(); //re-adds owner to maintain count/algorithm
+                    }
+                    if (countedOwner.ShipCount > 2) ships.Add(countedOwner.Ship);
+                    // more than two instances means the owner has been treated, and can be added directly,
+                    // bypassing the steps above which are designed for handling the first occurance of serial-ownership
+                }
+            }
+
+            foreach (var ship in ships)
+            {
+                //if (ship == null) continue;
+                Console.WriteLine($"{ship.Id } {ship.Name} {ship.Owner.Name} {ship.Owner.ShipCount}");
+            }
+        }
+    }
+}
